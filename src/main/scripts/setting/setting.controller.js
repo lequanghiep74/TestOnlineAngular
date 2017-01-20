f3432e000043e0548cf4edb3f09d92806887e752
hieplq <lequanghiep74@gmail.com>
2017-01-21 00:28:57 +0700

    Add model for setting form

--- src/main/scripts/setting/setting.controller.js
+++ src/main/scripts/setting/setting.controller.js
@@ -5,5 +5,11 @@
 
 angular.module('myApp')
     .controller('SettingController', function ($scope) {
-
+        $scope.status = {
+            select: [
+                {name: 'Not set', value: 'noteset'},
+                {name: 'True', value: "True"},
+                {name: 'False', value: "False"}
+            ]
+        };
     });
